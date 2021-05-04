bd1c263860af6a096ad3e88498d7b7e138213645
Ziya <ziauddinziya07@gmail.com>
2021-05-04 15:52:28 +0530

    Have done all the necessary changes

--- js/student_login.js
+++ js/student_login.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f0713b48107a27dbc3e567ee876c7d8e18a95499
+
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
@@ -12,9 +9,6 @@ sign_up_btn.addEventListener("click", () => {
 
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
-<<<<<<< HEAD
-});
-=======
 });
 
 function signup() {
@@ -35,20 +29,7 @@ function signup() {
   console.log(password);
   console.log(conpassword);
 
+
   alert("Sign up successful");
   document.querySelector("#sign-in-btn").click();
 }
-=======
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
-
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
-});
-
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
-});
->>>>>>> 9ff675bc4ecfdb599361e2bbcb8ddd370edec65e
->>>>>>> f0713b48107a27dbc3e567ee876c7d8e18a95499
