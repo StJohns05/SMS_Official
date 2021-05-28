3ea22f3fa50f0b78814537320dfac05a9ed1b7a0
Vidya-9 <vidyagolta509@gmail.com>
2021-05-28 11:50:13 +0530

    updated css and linkage is done

--- js/student_login.js
+++ js/student_login.js
@@ -2,15 +2,25 @@
 
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyDuu4FIsR0yRIkOrtu6T59lqfPbN7SG53M",
-  authDomain: "sms-demo-9ec85.firebaseapp.com",
-  databaseURL: "https://sms-demo-9ec85-default-rtdb.firebaseio.com",
-  projectId: "sms-demo-9ec85",
-  storageBucket: "sms-demo-9ec85.appspot.com",
-  messagingSenderId: "401316316472",
-  appId: "1:401316316472:web:2929d9673d92fbfbb66dcd"
+const firebaseConfig = {
+  apiKey: "AIzaSyB5AZ9E2DPj9TdFalJyU5zqekrnaOMYmUo",
+  authDomain: "student-information-syst-2384a.firebaseapp.com",
+  databaseURL: "https://student-information-syst-2384a-default-rtdb.firebaseio.com",
+  projectId: "student-information-syst-2384a",
+  storageBucket: "student-information-syst-2384a.appspot.com",
+  messagingSenderId: "805316303709",
+  appId: "1:805316303709:web:f964549876d5d34de666b4",
+  measurementId: "G-T3346KNRHT"
 };
+// var firebaseConfig = {
+//   apiKey: "AIzaSyDuu4FIsR0yRIkOrtu6T59lqfPbN7SG53M",
+//   authDomain: "sms-demo-9ec85.firebaseapp.com",
+//   databaseURL: "https://sms-demo-9ec85-default-rtdb.firebaseio.com",
+//   projectId: "sms-demo-9ec85",
+//   storageBucket: "sms-demo-9ec85.appspot.com",
+//   messagingSenderId: "401316316472",
+//   appId: "1:401316316472:web:2929d9673d92fbfbb66dcd"
+// };
 
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -103,6 +113,12 @@ function signup() {
         Password: password
       });
       document.querySelector("#signupForm").reset(); 
+      cred.user.updateProfile({
+        displayName: sname,
+        phoneNo:phoneno
+
+      });
+    
 
       alert("Sign up successful");
       document.querySelector("#sign-in-btn").click();
